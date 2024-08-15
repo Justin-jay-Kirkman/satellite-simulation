2fda4467fb7cdf96eb9c8ec0028aa6bfe89e2fe9
JK <justin.j.kirkman@gmail.com>
2024-08-14 21:07:58 -0400

    added django models

--- satellite_project/satellite_project/settings.py
+++ satellite_project/satellite_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'simulation_app'
+    'simulation_app',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [

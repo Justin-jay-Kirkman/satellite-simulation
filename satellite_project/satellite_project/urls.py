373c16a83de7590e9361c52e8a730ec961d86ac9
JK <justin.j.kirkman@gmail.com>
2024-08-14 23:44:44 -0400

    added temp landing page

--- satellite_project/satellite_project/urls.py
+++ satellite_project/satellite_project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from simulation_app.api import api
+from simulation_app.views import main_page
 
 urlpatterns = [
+    path('', main_page, name='main_page'),
     path('admin/', admin.site.urls),
     path('api/', api.urls),
 ]

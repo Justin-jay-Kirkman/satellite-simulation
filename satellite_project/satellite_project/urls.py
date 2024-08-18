6f611bf641af718c57ea80d8a2763b4c182be41a
JK <justin.j.kirkman@gmail.com>
2024-08-18 18:44:17 -0400

    vue auth boilerplate

--- satellite_project/satellite_project/urls.py
+++ satellite_project/satellite_project/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
     path('', include('simulation_app.urls')),
-    path('', include('vue.urls')),
+    path('', include('vue_login_app.urls')),
 ]

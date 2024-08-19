95b43cc7acf2a96f0dafa221dc0ec27533af57af
JK <justin.j.kirkman@gmail.com>
2024-08-19 01:04:47 -0400

    vue Set to Nominal

--- vue-frontend-project/src/pages/Simulation.vue
+++ vue-frontend-project/src/pages/Simulation.vue
@@ -4,6 +4,7 @@
 
     <button @click="startSSE()">Start</button>
     <button @click="stopSSE()" disabled>Stop</button>
+    <div class="alert-success" v-if="updateStatusMsg">{{updateStatusMsg}}</div>
     <table  class="table">
       <tr>
         <td>Name</td>
@@ -13,7 +14,7 @@
       <tr v-for="s in satellites" data-id="{{s.id}}">
         <td>{{s.name}}</td>
         <td>{{s.status}}</td>
-        <td data-id="{{s.id}}"><a @click="updateStatus()">update</a></td>
+        <td data-id="{{s.id}}"><button v-if="s.status =='MALFUNCTIONING'" @click="updateStatus(s.slug)">Set to Nominal</button></td>
       </tr>
     </table>
 
@@ -43,7 +44,8 @@ export default {
         status: "" // NOMINAL, MALFUNCTIONING
       }],
       sseData: [],
-      error: ""
+      error: "",
+      updateStatusMsg: "",
     }
   },
   methods: {
@@ -73,32 +75,27 @@ export default {
           this.eventSource.close();
         }
     },
-    async updateStatus(){
-        // try {
-        //   const response = await fetch('http://localhost:8000/api/register', {
-        //     method: 'POST',
-        //      headers: {
-        //               'Content-Type': 'application/json',
-        //               'X-CSRFToken': getCSRFToken()
-        //           },
-        //     body: JSON.stringify({
-        //       email: this.email,
-        //       password: this.password
-        //     }),
-        //     credentials: 'include'
-        //   })
-        //   const data = await response.json()
-        //   if (response.ok) {
-        //     this.success = 'Registration successful! Please log in.'
-        //     setTimeout(() => {
-        //       this.$router.push('/login')
-        //     }, 1000)
-        //   } else {
-        //     this.error = data.error || 'Registration failed'
-        //   }
-        // } catch (err) {
-        //   this.error = 'An error occurred during registration: ' + err
-        // }
+    async updateStatus(slug){
+        try {
+          const response = await fetch('http://localhost:8000/api/spacecrafts/' + slug + '/update-status', {
+            method: 'POST',
+             headers: {
+                      'Content-Type': 'application/json',
+                      'X-CSRFToken': getCSRFToken()
+                  },
+            body: JSON.stringify({
+              status: "NOMINAL"
+            }),
+          })
+          const data = await response.json()
+          if (response.ok) {
+            this.updateStatus = slug + "set to NOMINAL"
+          } else {
+            this.error = data.error || 'Update failed'
+          }
+        } catch (err) {
+          this.error = 'An error occurred during sateillite update for: ' + err
+        }
     }
   },
   async mounted() {
